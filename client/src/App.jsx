3a62f35389a8e443408347e3731433447ea9b017
krisprajapati03 <krisprajapati99@gmail.com>
2025-08-01 12:19:50 +0530

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className="bg-blue-500 text-black p-4 text-center">
+      App dflvkbwi
+    </div>
+  )
+}
+
+export default App
